d516ce5d7ee7ca145c7a5e34fea5a385ea5981d9
mariobox <mariosc@gmail.com>
2017-05-29 17:08:04 -0400

    clean jquery ajax call

--- src/js/formspree.js
+++ src/js/formspree.js
@@ -2,9 +2,7 @@ var $contactForm = $('#contact-form');
 
 $contactForm.submit(function(e) {
 	e.preventDefault();
-	var $submit = $('input:submit', $contactForm);
-	var defaultSubmitText = $submit.val();
-
+	
 	$.ajax({
 		url: '//formspree.io/mariosc@gmail.com',
 		method: 'POST',
@@ -14,11 +12,11 @@ $contactForm.submit(function(e) {
 			$('#howToContact').hide();
 			$('#message').html('<h4>Sending message…</h4>');
 		},
-		success: function(data) {
+		success: function() {
 			$('#howToContact').hide();
 			$('#message').html('<h4> <i class="fa fa-check-circle" aria-hidden="true" style="color:green;"></i> Your form has been sent successfully!</h4>');
 		},
-		error: function(err) {
+		error: function() {
 			$('#howToContact').hide();
 			$('#message').html('<h4><i class="fa fa-exclamation-triangle" aria-hidden="true" style="color:red;"></i> Ops, there was an error.<a href="index.html">Try again</a></h4>');
 			
